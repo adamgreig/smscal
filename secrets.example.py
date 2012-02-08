212c3ca01d609d40c154882bbf4018b35c366445
Adam Greig <adam@adamgreig.com>
2012-02-08 16:08:07 +0000

    Add MongoDB support

---
+++ secrets.example.py
@@ -0,0 +1,5 @@
+# Put Google API keys here, then rename to secrets.py
+
+g_client_id = ""
+g_client_secret = ""
+
--- smscal.py
+++ smscal.py
@@ -1,12 +1,30 @@
 import os
-from flask import Flask
+import sys
+import flask
+import pymongo
 
-app = Flask(__name__)
+def setup_mongo():
+    try:
+        uri = os.environ['MONGOLAB_URI']
+        dbname = uri.split('/')[-1]
+        print "Trying URI {0} and DB {1}...".format(uri, dbname)
+        con = pymongo.Connection(uri)
+        db = con[dbname]
+    except KeyError:
+        print "Could not find MONGOLAB_URI in env, quitting."
+        sys.exit(1)
+    return db
+
+app = flask.Flask(__name__)
+db = setup_mongo()
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    string = ''
+    for user in db.users.find():
+        string += str(user)
+    return string
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
